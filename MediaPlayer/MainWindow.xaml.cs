7308e345513dfa89513e6364e5ef39fe4cc73154
Tseng-Chih-Chung <chiao105031@gmail.com>
2023-05-15 15:20:54 +0800

    進度
    1.完成聲音大小的調整滑桿的介面及功能
    2.完成影片時間軸的滑桿介面及功能
    3.調整預設註解

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;//同時跑兩個程式
 
 namespace MediaPlayer
 {
@@ -38,30 +39,64 @@ namespace MediaPlayer
             {
                 txt_filePath.Text = filename;// 將檔案位置與檔名顯示在輸入文字框裡面
                 Uri u = new Uri(filename);// 將檔案位置與檔名轉化成URI(一種用來設定檔案資源定位的位置資料)
-                Med_show.Source = u;// 將URI放進影音元件中
-                Med_show.Volume = 1f; // 設定這個影音的聲音大小（可有可無）
+                Med_show.Source = u;// 將URI(檔案位置)放進影音元件中
+                Med_show.Volume = 1f; // 設定這個影音的聲音大小（可有可無）(0~1)
                 Med_show.LoadedBehavior = MediaState.Play;// 將影音進行播放
             }
         }
 
         private void btn_play_Click(object sender, RoutedEventArgs e)
         {
-            Med_show.LoadedBehavior = MediaState.Play;
+            Med_show.LoadedBehavior = MediaState.Play;//讓檔案的加載行為設為撥放
         }
 
         private void btn_pause_Click(object sender, RoutedEventArgs e)
         {
-            Med_show.LoadedBehavior = MediaState.Pause;
+            Med_show.LoadedBehavior = MediaState.Pause;//讓檔案的加載行為設為暫停
         }
 
         private void btn_stop_Click(object sender, RoutedEventArgs e)
         {
-            Med_show.LoadedBehavior = MediaState.Stop;
+            Med_show.LoadedBehavior = MediaState.Stop;//讓檔案的加載行為設為停止並從頭撥放
         }
 
         private void btn_exit_Click(object sender, RoutedEventArgs e)
         {
-            Environment.Exit(0);
+            Environment.Exit(0);//關閉視窗
         }
+
+        private void sliVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Med_show.Volume = sliVolume.Value; // 讓影片的聲音跟大小聲拖動處連動
+            //txt_filePath.Text = Med_show.Volume.ToString();// 讓音量數值具體顯示在上方的文字輸入框
+        }
+
+        TimeSpan TimePosition; // 宣告一個時間間格
+        DispatcherTimer timer = null; // 宣告一個「空的」計時器
+        private void Med_show_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            TimePosition = Med_show.NaturalDuration.TimeSpan; // 取得所開啟影片的時間長度
+            sliProgress.Minimum = 0;// 讓影片播放滑桿從零開始
+            sliProgress.Maximum = TimePosition.TotalMilliseconds; //最大值設定為影片的總毫秒數(該影音時長)
+
+            timer = new DispatcherTimer();// 設定計時器
+            timer.Interval = TimeSpan.FromSeconds(1); // 這個計時器設定每一個刻度為1秒(1秒跑1次)
+            timer.Tick += new EventHandler(timer_tick); //每一個時間刻度設定一個小程序timer_tick
+            timer.Start(); // 啟動這個計時器
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            sliProgress.Value = Med_show.Position.TotalMilliseconds;// 小程序，更新目前影片播放進度(顯示當前進度)
+        }
+
+        private void sliProgress_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int SliderValue = (int)sliProgress.Value; // 還記得轉型嗎？
+
+            TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue); //將滑桿的數值改變成時間間格的資料形式
+            Med_show.Position = ts; // 調整影片播放進度到新的時間
+        }
+
     }
 }
